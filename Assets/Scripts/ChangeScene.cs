8c39ff17b90440e23b7dad6e4ecd4ab5f9145b85
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2021-07-29 00:52:24 -0400

    starter

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void SwapToScene(string sceneName) {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
