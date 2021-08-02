b963d33be3e8f7ac2fb5ac1a230f5445b6f84102
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2021-08-01 20:20:49 -0400

    base functionality

---
+++ Assets/SHOP_OpenBox.cs
@@ -0,0 +1,153 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using TMPro;
+using UnityEngine.UI;
+
+public class SHOP_OpenBox : MonoBehaviour
+{
+    [SerializeField] SkeletonGraphic chestSkeleton;
+    [SerializeField] TextMeshProUGUI rewardName;
+    [SerializeField] TextMeshProUGUI rewardDesc;
+    [SerializeField] Image rewardIcon;
+
+    [Header("Sprites")]
+    [SerializeField] Sprite spr_Coins;
+    [SerializeField] Sprite spr_Belt;
+    [SerializeField] Sprite spr_Chest;
+    [SerializeField] Sprite spr_Helm;
+   
+
+    GameController_DDOL _gc;
+    string _text;
+    string _desc;
+    Sprite _sprite;
+
+    private void Start()
+    {
+        _gc = FindObjectOfType<GameController_DDOL>();
+        rewardIcon.gameObject.SetActive(false);
+        rewardName.gameObject.SetActive(false);
+        rewardDesc.gameObject.SetActive(false);
+    }
+    bool isOpen = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        if (isOpen)
+        {
+            GetComponent<ChangeScene>().SwapToScene("Store");
+            return;
+        }
+
+        int reward = Random.Range(0, 101);
+
+        if (reward <= 75)
+        {
+            AwardRandomCoins();
+        } else if (reward <= 95)
+        {
+            AwardRandomUpgrade();
+        } else
+        {
+            AwardRandomItem();
+        }
+
+        StartCoroutine("DoOpen");
+    }
+
+    void AwardRandomCoins()
+    {
+        int coinCt = Random.Range(15, 56);
+        _text = coinCt + " Coins";
+        _desc = "Better luck next time";
+        _sprite = spr_Coins;
+        _gc.CoinBalanceChange(coinCt);
+    }
+
+    void AwardRandomUpgrade()
+    {
+        int randomId = Random.Range(1, 9);
+        while (randomId == 5)
+        {
+            randomId = Random.Range(1, 9);
+        }
+ 
+        CharacterUpgrade rando = Resources.Load<CharacterUpgrade>("CharacterUpgrade/" + randomId);
+        _text = rando.title;
+        _desc = rando.description;
+        _sprite = Resources.Load<Sprite>("Images/" + rando.iconName);
+        _gc.RequestCharacterUpgrade(rando);
+    }
+
+    void AwardRandomItem()
+    {
+        if (_gc.hasBelt && _gc.hasChestplate && _gc.hasHelmet)
+        {
+            AwardRandomUpgrade();
+            return;
+        }
+
+        _desc = "A rare find!";
+        bool foundOne = false;
+        while (!foundOne)
+        {
+            int random = Random.Range(1, 4);
+            switch (random)
+            {
+                case 1:
+                    if (!_gc.hasBelt)
+                    {
+                        // give em the belt
+                        _text = "Belt of Thievery";
+                        _sprite = spr_Belt;
+                        _gc.hasBelt = true;
+                        foundOne = true;
+                    }
+                    break;
+                case 2:
+                    if (!_gc.hasChestplate)
+                    {
+                        // give em the chest
+                        _text = "Helm of Conscription";
+                        _sprite = spr_Helm;
+                        _gc.hasHelmet = true;
+                        foundOne = true;
+                    }
+                    break;
+                default:
+                    if (!_gc.hasHelmet)
+                    {
+                        // give em the helmet
+                        _text = "Chestplate of Warding";
+                        _sprite = spr_Chest;
+                        _gc.ObtainChestplate();
+                        foundOne = true;
+                    }
+                    break;
+            }
+        }
+    }
+
+    IEnumerator DoOpen()
+    {
+        isOpen = true;
+        chestSkeleton.AnimationState.SetAnimation(0, "open", false);
+        rewardIcon.sprite = _sprite;
+        rewardDesc.text = _desc;
+        rewardName.text = _text;
+        yield return new WaitForSeconds(0.25f);
+        rewardIcon.gameObject.SetActive(true);
+        rewardName.gameObject.SetActive(true);
+        rewardDesc.gameObject.SetActive(true);
+    }
+}
--- Assets/SHOP_ShopKeep.cs
+++ Assets/SHOP_ShopKeep.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SHOP_ShopKeep : MonoBehaviour
 {
     GameController_DDOL _gc;
     [SerializeField] Button mb;
     [SerializeField] Button helm;
+    [SerializeField] TextMeshProUGUI helmPrice;
     [SerializeField] Button chest;
+    [SerializeField] TextMeshProUGUI chestPrice;
     [SerializeField] Button belt;
+    [SerializeField] TextMeshProUGUI beltPrice;
+
 
     void Start()
     {
@@ -18,42 +23,69 @@ public class SHOP_ShopKeep : MonoBehaviour
 
     private void FixedUpdate()
     {
+        int price = getGoingRate();
+
         mb.interactable = _gc.coins >= 200;
 
-        helm.interactable = _gc.coins >= 1000 && !_gc.hasHelmet;
-        chest.interactable = _gc.coins >= 1000 && !_gc.hasChestplate;
-        belt.interactable = _gc.coins >= 1000 && !_gc.hasBelt;
+        helm.interactable = _gc.coins >= price && !_gc.hasHelmet;
+        chest.interactable = _gc.coins >= price && !_gc.hasChestplate;
+        belt.interactable = _gc.coins >= price && !_gc.hasBelt;
+
+        helmPrice.text = _gc.hasHelmet ? "OWNED" : "" + price;
+        chestPrice.text = _gc.hasChestplate ? "OWNED" : "" + price;
+        beltPrice.text = _gc.hasBelt ? "OWNED" : "" + price;
+    }
+
+    int getGoingRate()
+    {
+        int owned = 0;
+        if (_gc.hasHelmet) owned += 1;
+        if (_gc.hasChestplate) owned += 1;
+        if (_gc.hasBelt) owned += 1;
+
+        int price = 0;
+        switch (owned)
+        {
+            case 0: price = 500; break;
+            case 1: price = 1000; break;
+            default: price = 1500; break;
+
+        }
+        return price;
     }
 
     public void AttemptToPurchaseMysteryBox()
     {
-        if (_gc.coins < 200) return;
+        if (_gc.coins < 100) return;
 
-        Debug.Log("BOUGHT MYSTERY BOX, rip its empty");
-        _gc.CoinBalanceChange(-200);
+        _gc.CoinBalanceChange(-100);
+        _gc.ChangeScene("OpenMysteryBox");
     }
 
     public void AttemptToPurchaseHelmet()
     {
-        if (_gc.coins < 1000 || _gc.hasHelmet) return;
+        int price = getGoingRate();
+        if (_gc.coins < price || _gc.hasHelmet) return;
 
         _gc.hasHelmet = true;
-        _gc.CoinBalanceChange(-1000);
+        _gc.CoinBalanceChange(-price);
     }
 
     public void AttemptToPurchaseChestplate()
     {
-        if (_gc.coins < 1000 || _gc.hasChestplate) return;
+        int price = getGoingRate();
+        if (_gc.coins < price || _gc.hasChestplate) return;
 
         _gc.ObtainChestplate();
-        _gc.CoinBalanceChange(-1000);
+        _gc.CoinBalanceChange(-price);
     }
 
     public void AttemptToPurchaseBelt()
     {
-        if (_gc.coins < 1000 || _gc.hasBelt) return;
+        int price = getGoingRate();
+        if (_gc.coins < price || _gc.hasBelt) return;
 
         _gc.hasBelt = true;
-        _gc.CoinBalanceChange(-1000);
+        _gc.CoinBalanceChange(-price);
     }
 }
